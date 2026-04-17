466b3a1bf3c40f60a66dfc0e11e129674c04c7b4
Icyhuman <jorgen.storum@gmail.com>
2026-04-17 20:52:50 +0200

    Website work

--- finalwebpage/script.js
+++ finalwebpage/script.js
@@ -1,5 +1,5 @@
 function byebye(img){
-    console.log(img)
+    console.log('removing image')
     let image=img
     image.classList.add("boxgo")
     setTimeout(() => {
@@ -9,8 +9,12 @@ function byebye(img){
 }
 
 function pinclick(pin, link){
+    console.log('Displaying image: '+link)
     let box=document.getElementById("box")
     box.src=link
     box.style.display="block"
     pin.src="imgs/gpin.png"
 }
+
+console.log('Oh hey, you\'re checking out the console. Cool. Not that there\'s much here, unless you\'re really into actions being logged.')
+let secret = 'Yahaha! You found me!'
